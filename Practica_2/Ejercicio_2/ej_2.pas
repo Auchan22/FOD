76dd975b5f3cc72b84b3ba2d8b3baa52f2b3c31d
Auchan22 <agustinsurila@gmail.com>
2024-03-16 17:27:14 -0300

    Ejercicio 2 p2

--- Practica_2/Ejercicio_2/ej_2.pas
+++ Practica_2/Ejercicio_2/ej_2.pas
@@ -78,9 +78,167 @@ begin
 	close(arch_d);
 	close(arch_m);
 end;
-procedure ShowMenu(opc: char; var arch: maestro)
+procedure crearMaestro(var arch: maestro);
+	procedure leerAlumno(var a: alumno);
+	begin
+		writeln('-----');
+		write('Ingresar codigo: ');
+		readln(a.codigo);
+		if(a.codigo <> -1) then begin
+			write('Ingresar Nombre: ');
+			readln(a.nombre);
+			write('Ingresar Apellido: ');
+			readln(a.apellido);
+			write('Ingresar n° materias aprobadas sin finalo: ');
+			readln(a.cantMsin);
+			write('Ingresar n° materias aprobadas con final: ');
+			readln(a.cantMcon);
+		end;
+		writeln('-----');
+	end;
+var
+	a: alumno;
+begin
+	writeln('-- Inicio Carga -- ');
+	rewrite(arch);
+	leerAlumno(a);
+	while(a.codigo <> -1) do begin
+		write(arch, a);
+		leerAlumno(a);
+	end;
+	close(arch);
+	writeln('-- Fin Carga -- ');
+end;
+procedure crearDetalle(var arch: detalle);
+	procedure leerAlumno(var a: a_resumen);
+	begin
+		writeln('-----');
+		write('Ingresar codigo de alumno: ');
+		readln(a.codigo);
+		if(a.codigo <> -1) then begin
+			write('Ingresar materia: ');
+			readln(a.nombre);
+			write('Ingresar 1 si aprobo cursada: ');
+			readln(a.cursada);
+			write('Ingresar 1 si aprobo final: ');
+			readln(a.fin);
+		end;
+		writeln('-----');
+	end;
+var
+	a: a_resumen;
+begin
+	writeln('-- Inicio Carga -- ');
+	rewrite(arch);
+	leerAlumno(a);
+	while(a.codigo <> -1) do begin
+		write(arch, a);
+		leerAlumno(a);
+	end;
+	close(arch);
+	writeln('-- Fin Carga -- ');
+end;
+procedure imprimirMaestro(var arch_m: maestro);
+	procedure ImprimirAlumno(a: alumno);
+	begin
+		writeln('-----');
+		writeln('Codigo: ', a.codigo);
+		writeln('Nombre: ', a.nombre);
+		writeln('Apellido: ', a.apellido);
+		writeln('Cant Materias aprobadas sin final: ', a.cantMsin);
+		writeln('Cant Materias aprobadas con final: ', a.cantMcon);
+		writeln('-----');
+	end;
+var
+	a: alumno;
+begin
+	reset(arch_m);
+	while(not eof(arch_m))do begin
+		read(arch_m, a);
+		ImprimirAlumno(a);
+	end;
+	close(arch_m);
+end;
+procedure generarArchivo(var arch_m: maestro);
+var
+	txt: Text;
+	a: alumno;
+begin
+	Assign(txt, 'listado_cursadas.txt');
+	reset(arch_m);
+	rewrite(txt);
+	writeln(txt, 'CODIGO',' ', 'NOMBRE',' ', 'APELLIDO',' ', 'N° APROBADAS SIN FINAL',' ', 'N° APROBADAS CON FINAL');
+	while(not eof(arch_m)) do begin
+		read(arch_m, a);
+		if(a.cantMsin >= 4) then
+			writeln(txt,a.codigo,' ',a.nombre,' ',a.apellido,' ',a.cantMsin,' ',a.cantMcon);
+	end;
+	close(txt);
+	close(arch_m);
+end;
+procedure ShowMenu(var arch_m: maestro; var arch_d: detalle);
+var
+	opc: char;
+begin
+	writeln('------');
+	writeln('');
+	writeln('[a] Crear archivo maestro');
+	writeln('[b] Crear archivo detalle');
+	writeln('[c] Listar maestro');
+	writeln('[d] Listar detalle');
+	writeln('[e] Actualizar maestro');
+	writeln('[f] Generar archivo de texto');
+	writeln('');
+	readln(opc);
+	writeln('OPCION ELEGIDA -->  ', opc);
+	writeln('------');
+	
+	case opc of
+		'a':
+		begin
+			crearMaestro(arch_m);
+		end;
+		'b':
+		begin
+			crearDetalle(arch_d);
+		end;
+		'c':
+		begin
+			imprimirMaestro(arch_m);
+		end;
+		'd':
+		begin
+		
+		end;
+		'e': 
+		begin
+			actualizar(arch_m, arch_d);
+		end;
+		'f':
+		begin
+			generarArchivo(arch_m);
+		end;
+		else writeln('No se encuentra la opción...');
+	end;
+end;
+var
+	arch_m: maestro;
+	arch_d: detalle;
+	loop: boolean;
+	letra: char;
 BEGIN
-	
-	
+	Assign(arch_m, 'alumnos.dat');
+	Assign(arch_d, 'alumnos_detalle.dat');
+	loop:= true;
+	ShowMenu(arch_m, arch_d);
+	while (loop) do begin
+		write ('SI INGRESA CUALQUIER CARACTER SE DESPLEGARA NUEVAMENTE EL MENU. SI INGRESA "Z" SE CERRARA LA CONSOLA: '); readln (letra);
+		if (letra = 'Z') or (letra = 'z') then
+			loop:= false
+		else begin
+			clrscr;
+			ShowMenu(arch_m, arch_d);
+		end;
+	end;
 END.
 
