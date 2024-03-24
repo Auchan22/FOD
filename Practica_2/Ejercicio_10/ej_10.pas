2f2e00b9b07ffef2470850a7b0f358c242ed4faa
Auchan22 <agustinsurila@gmail.com>
2024-03-24 17:58:42 -0300

    ej 10 p2

--- Practica_2/Ejercicio_10/ej_10.pas
+++ Practica_2/Ejercicio_10/ej_10.pas
@@ -11,6 +11,8 @@ continuación:
 
 program ej10_p2;
 uses crt;
+const
+	valoralto = 9999;
 type
 	acceso = record
 		anio: integer;
@@ -29,14 +31,103 @@ procedure cargarArchivo(var arch: archivo);
 		write('Ingrese anio: ');
 		readln(a.anio);
 		if(a.anio <> -1) then begin
-			write('Ingrese anio: ');
-			readln(a.anio);
+			write('Ingrese mes: ');
+			readln(a.mes);
+			write('Ingrese dia: ');
+			readln(a.dia);
+			write('Ingrese idUsuario: ');
+			readln(a.idUsuario);
+			write('Ingrese tiempo: ');
+			readln(a.tiempo);
 		end;
 		writeln('-----');
 	end;
+var
+	a: acceso;
+begin
+	writeln('-- Carga archivo --');
+	rewrite(arch);
+	leerAcceso(a);
+	while a.anio <> -1 do begin
+		write(arch, a);
+		leerAcceso(a);
+	end;
+	close(arch);
+	writeln('-----');
+end;
 
-BEGIN
+procedure leer(var arch: archivo; var dato: acceso);
+begin
+	if(not eof(arch)) then
+		read(arch, dato)
+	else
+		dato.anio := valoralto;
+end;
+
+procedure generarInforme(var arch: archivo);
+var
+	a: acceso;
+	existe: boolean;
+	anio: integer;
+	tot_dia, tot_mes, tot_usr, tot: real;
+	anio_actual, mes_actual, dia_actual, usr_actual: integer;
+begin
+	writeln('-- Generar Informe --');
+	writeln('');
+	write('-> Ingrese anio a buscar: ');
+	readln(anio);
+	existe:= false;
+	reset(arch);
+	leer(arch, a);
+	while(a.anio <> valoralto) and (not existe) do begin
+		if(a.anio = anio) then
+			existe:= true
+		else
+			leer(arch, a);
+	end;
 	
+	if(existe) then begin
+		seek(arch, filepos(arch) - 1);
+		leer(arch, a);
+		writeln('Anio: ', a.anio);
+		anio_actual := a.anio;
+		tot:= 0;
+		while(a.anio = anio_actual) do begin
+			mes_actual:= a.mes;
+			tot_mes:= 0;
+			while(a.anio = anio_actual) and (a.mes = mes_actual) do begin
+				writeln(' Mes: ', mes_actual);
+				dia_actual:= a.dia;
+				tot_dia:= 0;
+				while(a.anio = anio_actual) and (a.mes = mes_actual) and (a.dia = dia_actual) do begin
+					writeln('  Dia ', dia_actual);
+					usr_actual:= a.idUsuario;
+					tot_usr:= 0;
+					while(a.anio = anio_actual) and (a.mes = mes_actual) and (a.dia = dia_actual)and (a.idUsuario = usr_actual) do begin
+						tot_usr:= tot_usr + a.tiempo;
+						leer(arch, a);
+					end;
+					writeln('   idUsuario: ', usr_actual, ' Tiempo total de acceso en el dia ', dia_actual, ' mes ',mes_actual, ': ', tot_usr:0:2);
+					tot_dia:= tot_dia + tot_usr
+				end;
+				writeln('  Tiempo total acceso dia ', dia_actual, ' mes ', mes_actual, ': ', tot_dia:0:2);
+				tot_mes:= tot_mes + tot_dia;
+			end;
+			writeln(' Total tiempo de acceso mes: ',mes_actual, ': ', tot_mes:0:2);
+			tot:= tot + tot_mes;
+		end;
+		writeln('Total tiempo de acceso anio: ',anio_actual, ': ',tot:0:2);
+	end
+	else
+		writeln('Anio no encontrado :(');
+	close(arch);
+end;
 
+var
+	arch: archivo;
+BEGIN
+	Assign(arch, 'archivo');
+	//cargarArchivo(arch);
+	generarInforme(arch);
 END.
 
