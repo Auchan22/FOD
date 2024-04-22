4a5af8fe8f294af278f5f5cc960d406f83219e42
Auchan22 <agustinsurila@gmail.com>
2024-04-22 11:00:35 -0300

    Ej 8 y 1p2 p3

---
+++ Practica_3/Ejercicio1_Parte2/ej1_p2.pas
@@ -0,0 +1,24 @@
+{}
+
+program ej1_2_p3;
+
+uses crt;
+type
+	producto = record
+		cod: integer;
+		nombre: string[20];
+		precio: real;
+		stockActual: integer;
+		stockMinimo: integer;
+	end;
+	
+	venta = record
+		cod: integer;
+		cantVendida: integer;
+	end;
+
+BEGIN
+	
+	
+END.
+
--- Practica_3/Ejercicio_8/ej_8.pas
+++ Practica_3/Ejercicio_8/ej_8.pas
@@ -55,7 +55,7 @@ begin
 	leer(a, d);
 	existe := false;
 	while (d.nombre <> valoralto) and (not existe) do begin
-		if(d.nombre = n) then
+		if(d.nombre = n) and (d.cantD > 0) then
 			existe:= true
 		else
 			leer(a, d);
@@ -98,11 +98,160 @@ begin
 	close(a);
 end;
 
+procedure AltaDistribucion(var a: archivo);
+var
+	d: distribucion;
+	cabecera, act: distribucion;
+	pos: integer;
+begin
+	writeln('-----');
+	writeln('-- Agregar Distribucion --');
+	writeln('');
+	reset(a);
+	leerDistribucion(d);
+	if(ExisteDistribucion(a, d.nombre)) then
+		writeln('Ya existe la distribucion')
+	else begin
+		seek(a, 0);
+		read(a, cabecera);
+		pos:= cabecera.cantD * -1;
+		if(pos > 0) then begin
+			seek(a, pos);
+			read(a, act);
+			seek(a, filepos(a) - 1);
+			cabecera:= act;
+			write(a, d);
+			seek(a, 0);
+			write(a, cabecera);
+		end
+		else begin
+			seek(a, filesize(a));
+			write(a, d);
+		end;
+		writeln('Se agrego la distribucion');
+	end;
+	close(a);
+	writeln('-----');
+end;
+
+procedure BajaDistribucion(var a: archivo);
+var
+	n: TString;
+	cabecera, act: distribucion;
+	pos: integer;
+begin
+	writeln('-----');
+	reset(a);
+	read(a, cabecera);
+	write('Ingrese nombre de distribucion: ');readln(n);
+	if(ExisteDistribucion(a, n)) then begin
+		pos:= filepos(a) -1; //Almaceno la pos del registro eliminado;
+		seek(a, pos); // Me posiciono en la ubi del archivo
+		read(a, act); // Lo almaceno en act
+		seek(a, pos); // Me vuelvo a posicionar
+		act.cantD := cabecera.cantD; // Actualizo la ubi del prox elemento disponible con lo que almacena cabecera
+		cabecera.cantD:= pos * -1; // Actualizo cabecera con la ult ubi disponible (la recien eliminada)
+		write(a, act); // Guardo el elemento eliminado
+		seek(a, 0); // Me posiciono en la cabecera
+		write(a, cabecera); // Guardo la cabecera con el nuevo index disponible
+	end
+	else
+		writeln('Distribucion no existente');
+	close(a);
+	writeln('-----');
+end;
+
+procedure imprimirArchivo(var a: archivo);
+	procedure imprimirDistribucion(d: distribucion);
+	begin
+		writeln('-----');
+		writeln('-> Nombre: ', d.nombre);
+		writeln('-> Descripcion: ', d.descripcion);
+		writeln('-> Anio Lanzamiento: ', d.anioLanzamiento);
+		writeln('-> Version Kernel: ', d.nroKernel);
+		writeln('-> Cant de desarrolladores: ', d.cantD);
+		writeln('-----');
+	end;
+var
+	d: distribucion;
+begin
+	reset(a);
+	leer(a, d);
+	while(d.nombre <> valoralto) do begin
+		//if(d.cantD >= 0) then
+			imprimirDistribucion(d);
+		leer(a, d);
+	end;
+	close(a);
+end;
+
+procedure ShowMenu(var arch_log: archivo);
+var
+	opc: byte;
+	n: TString;
+begin
+	writeln('------');
+	writeln('Ingrese por teclado la opción a efectuar: ');
+	writeln('');
+	writeln('[0] Cargar el archivo');
+	writeln('[1] Verificar existencia de distribucion');
+	writeln('[2] Agregar Distribucion');
+	writeln('[3] Eliminar Distribucion');
+	writeln('[4] Imprimir Archivo');
+	writeln('');
+	readln(opc);
+	writeln('OPCION ELEGIDA -->  ', opc);
+	writeln('------');
+	
+	case opc of
+		0:
+		begin
+			cargarArchivo(arch_log);
+		end;
+		1:
+		begin
+			writeln('-----');
+			write('Ingrese nombre de distribucion a verificar: ');readln(n);
+			reset(arch_log);
+			if(ExisteDistribucion(arch_log, n))then
+				writeln('Existe la distribucion')
+			else
+				writeln('No existe la distribución');
+			close(arch_log);
+			writeln('-----');
+		end;
+		2: 
+		begin
+			AltaDistribucion(arch_log);
+		end;
+		3:
+		begin
+			BajaDistribucion(arch_log);
+		end;
+		4:
+		begin
+			imprimirArchivo(arch_log);
+		end;
+		else writeln('No se encuentra la opción...');
+	end;
+end;
+
 var
 	arch: archivo;
+	loop: boolean;
+	letra: char;
 BEGIN
+	loop:= true;
 	assign(arch, 'maestro.data');
-	cargarArchivo(arch);
-	
+	ShowMenu(arch);
+	while (loop) do begin
+		write ('SI INGRESA CUALQUIER CARACTER SE DESPLEGARA NUEVAMENTE EL MENU. SI INGRESA E SE CERRARA LA CONSOLA: '); readln (letra);
+		if (letra = 'E') or (letra = 'e') then
+			loop:= false
+		else begin
+			clrscr;
+			ShowMenu(arch);
+		end;
+	end;
 END.
 
